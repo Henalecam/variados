7a25ab861f272b5fa00e8646e7128a055ac75234
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-06-22 14:14:58 -0300

    Iniciando página fixa

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+import { CuboPerfil } from './components/CuboPerfil'
+
+import './App.css'
+
+
+export   function App() {
+  
+  return (
+    <>
+    <h1>hello</h1>
+    <CuboPerfil/>
+
+    
+    </>
+    
+  )
+}
---
+++ Portfolio/src/components/CuboPerfil.tsx
@@ -0,0 +1,16 @@
+import './CuboPerfil.css';
+
+export function CuboPerfil() {
+  return(
+  <div id="cube-container">
+    <div id="cube">
+      <div className="face"> henrique henrique henrique</div>
+      <div className="face"></div>
+      <div className="face"></div>
+      <div className="face"></div>
+      <div className="face"></div>
+      <div className="face"></div>
+    </div>
+  </div>
+  )
+  }
