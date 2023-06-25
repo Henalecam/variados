248b8b682c552129b49e0407231794236ddd1552
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-06-25 19:01:30 -0300

    Updating

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,8 +4,16 @@ import Random from "./components/Random";
 function App() {
   return (
     <div className="">
+      <div className="relative items-start flex flex-col justify-between overflow-hidden border-r border-white/10 bg-[url(./assets/stars.svg)] bg-cover px-28 py-16">    
+            {/* Blur */}
+            <div className="absolute right-0 bottom-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1 rounded-full bg-palette-primary opacity-75 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-4 bg-stripes" />
+            <div className="absolute bottom-0 left-2 top-0 w-4 bg-stripes" />
       <Hero />
       <Random />
+      </div>
     </div>
 
   );
--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -3,38 +3,37 @@ import Gear from '../assets/gear.svg';
 import Github from '../assets/github.svg';
 import Linkedin from '../assets/linkedin.svg';
 import Whatsapp from '../assets/whatsapp.svg';
-import Mancha from '../assets/mancha.png';
 
 function Hero() {
   return (
-    <div className="h-screen border items-center justify-center bg-opacity-10 bg-gradient-to-b bg-stripes from-palette-secondary via-palette-tertiary ">
-      <p className="flex flex-col items-center justify-center pt-40 ">
+    <div className="border flex flex-row w-full items-center justify-center">
         <img
           src={Henrique}
           alt="Henrique"
-          className=" h-[200px] w-[200px] rounded-full border border-palette-tertiary border-2 shadow-2xl shadow-inner border-b-black bg-gradient-to-tr from-palette-primary via-palette-secondary to-transparent"
+          className=" h-[400px] w-[400px] rounded-full border border-palette-tertiary border-2 shadow-2xl shadow-inner border-b-black bg-gradient-to-tr from-palette-primary via-palette-secondary to-transparent"
         />
-        <h1 className="inline-block bg-gradient-to-r from-palette-quaternary via-orange-200 to-palette-quinary bg-clip-text text-4xl font-bold text-transparent">
+      <p className="flex flex-col ml-12 items-center justify-center pt-40 ">
+        <h1 className="inline-block font-normal bg-gradient-to-r from-palette-quaternary via-orange-200 to-palette-quinary bg-clip-text text-6xl font-bold text-transparent">
           Henrique Alexandre de Camargo
         </h1>
-        <h2 className="text-2xl font-bold font-extralight text-palette-quaternary">
+        <h2 className="text-4xl font-bold font-extralight text-palette-quaternary">
           {' '}
           <span className="text-palette-quinary">
             <img className="inline-block w-[6.6%]" src={Gear} alt="" /> Back-End
           </span>{' '}
           Developer
         </h2>
-      </p>
       <p className="mr-64 mt-4 flex items-center justify-center">
-        <a href="https://linkedin.com/in/henalecam" target='_blank' rel='noreferrer'>
-        <img className="mr-10 w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Linkedin} alt="" />
+        <a className='mr-10' href="https://linkedin.com/in/henalecam" target='_blank' rel='noreferrer'>
+        <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Linkedin} alt="" />
         </a>
-        <a href="https://github.com/henalecam" target='_blank' rel='noreferrer'>
-        <img className="mr-10 w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Github} alt="" />
+        <a className='mr-10' href="https://github.com/henalecam" target='_blank' rel='noreferrer'>
+        <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Github} alt="" />
         </a>
         <a href="https://wa.me/+5542998188268" target='_blank' rel='noreferrer'>
         <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Whatsapp} alt="" />
         </a>
+      </p>
       </p>
     </div>
   );
