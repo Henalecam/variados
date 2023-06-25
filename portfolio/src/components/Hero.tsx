c30b74027cf9a662a4afe1b130bb4fc2fab3ff51
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-06-25 01:44:50 -0300

    Ideia1

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -1,15 +1,43 @@
-import Henrique from '../assets/henrique.png'
+import Henrique from '../assets/henrique.png';
+import Gear from '../assets/gear.svg';
+import Github from '../assets/github.svg';
+import Linkedin from '../assets/linkedin.svg';
+import Whatsapp from '../assets/whatsapp.svg';
+import Mancha from '../assets/mancha.png';
 
 function Hero() {
   return (
-    <div>
-      <div className='flex flex-col  items-center justify-center h-[500px]'>
-        <img src={Henrique} alt="Henrique" className=' bg-gradient-to-tr from-cyan-700 via-gray-700 to-transparent w-[200px] h-[200px] rounded-full' />
-        <h1 className='text-4xl font-bold bg-gradient-to-r from-teal-700 via-cyan-700 to-sky-700 inline-block text-transparent bg-clip-text'>Henrique Alexandre de Camargo</h1>
-        <h2 className='text-2xl font-bold text-cyan-500'><span className='text-sky-700'>Back-End</span> Developer</h2>
-      </div>
+    <div className="h-screen border items-center justify-center bg-opacity-10 bg-gradient-to-b bg-stripes from-palette-secondary via-palette-tertiary ">
+      <p className="flex flex-col items-center justify-center pt-40 ">
+        <img
+          src={Henrique}
+          alt="Henrique"
+          className=" h-[200px] w-[200px] rounded-full border border-palette-tertiary border-2 shadow-2xl shadow-inner border-b-black bg-gradient-to-tr from-palette-primary via-palette-secondary to-transparent"
+        />
+        <h1 className="inline-block bg-gradient-to-r from-palette-quaternary via-orange-200 to-palette-quinary bg-clip-text text-4xl font-bold text-transparent">
+          Henrique Alexandre de Camargo
+        </h1>
+        <h2 className="text-2xl font-bold font-extralight text-palette-quaternary">
+          {' '}
+          <span className="text-palette-quinary">
+            <img className="inline-block w-[6.6%]" src={Gear} alt="" /> Back-End
+          </span>{' '}
+          Developer
+        </h2>
+      </p>
+      <p className="mr-64 mt-4 flex items-center justify-center">
+        <a href="https://linkedin.com/in/henalecam" target='_blank' rel='noreferrer'>
+        <img className="mr-10 w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Linkedin} alt="" />
+        </a>
+        <a href="https://github.com/henalecam" target='_blank' rel='noreferrer'>
+        <img className="mr-10 w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Github} alt="" />
+        </a>
+        <a href="https://wa.me/+5542998188268" target='_blank' rel='noreferrer'>
+        <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Whatsapp} alt="" />
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'palette': {
+          'primary': '#0554f2',
+          'secondary': '#056cf2',
+          'tertiary': '#034aa6',
+          'quaternary': '#07dbf2',
+          'quinary': '#05f2f2'
+        }
+      },
       backgroundImage: {
         stripes:
           'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
