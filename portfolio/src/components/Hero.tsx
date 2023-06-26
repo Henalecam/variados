593feece4e77fc5755d1352a6a29a34ba5bb9e3d
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-06-25 21:20:04 -0300

    Testando portifas

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -6,23 +6,24 @@ import Whatsapp from '../assets/whatsapp.svg';
 
 function Hero() {
   return (
-    <div className="border flex flex-row w-full items-center justify-center">
+    <div className="border items-center justify-center w-full">
+      <p className="flex flex-col items-center justify-center pt-40 ">
         <img
           src={Henrique}
           alt="Henrique"
-          className=" h-[400px] w-[400px] rounded-full border border-palette-tertiary border-2 shadow-2xl shadow-inner border-b-black bg-gradient-to-tr from-palette-primary via-palette-secondary to-transparent"
+          className=" h-[200px] w-[200px] rounded-full border border-palette-tertiary border-2 shadow-2xl shadow-inner border-b-black bg-gradient-to-b"
         />
-      <p className="flex flex-col ml-12 items-center justify-center pt-40 ">
-        <h1 className="inline-block font-normal bg-gradient-to-r from-palette-quaternary via-orange-200 to-palette-quinary bg-clip-text text-6xl font-bold text-transparent">
+        <h1 className="inline-block bg-gradient-to-r from-palette-quaternary via-orange-200 to-palette-quinary bg-clip-text text-4xl font-bold text-transparent">
           Henrique Alexandre de Camargo
         </h1>
-        <h2 className="text-4xl font-bold font-extralight text-palette-quaternary">
+        <h2 className="text-2xl font-bold  font-extralight text-palette-quaternary">
           {' '}
           <span className="text-palette-quinary">
             <img className="inline-block w-[6.6%]" src={Gear} alt="" /> Back-End
           </span>{' '}
           Developer
         </h2>
+      </p>
       <p className="mr-64 mt-4 flex items-center justify-center">
         <a className='mr-10' href="https://linkedin.com/in/henalecam" target='_blank' rel='noreferrer'>
         <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Linkedin} alt="" />
@@ -33,7 +34,6 @@ function Hero() {
         <a href="https://wa.me/+5542998188268" target='_blank' rel='noreferrer'>
         <img className="w-10 border border-palette-secondary shadow-xl hover:border-2 rounded-full" src={Whatsapp} alt="" />
         </a>
-      </p>
       </p>
     </div>
   );
